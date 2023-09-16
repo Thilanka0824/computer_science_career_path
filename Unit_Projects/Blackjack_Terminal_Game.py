99be8c5886654eb22955aa127c3bc4c5f6d9755e
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-09-15 22:10:21 -0700

    create deal method

--- Unit_Projects/Blackjack_Terminal_Game.py
+++ Unit_Projects/Blackjack_Terminal_Game.py
@@ -1,3 +1,5 @@
+import random 
+
 class Player:
     def __init__(self, score):
         self.score = score
@@ -6,92 +8,30 @@ class Player:
 
 
 class Cards:
-    deck = {
-            {
-            "suit": "Spades",
-            "card": {
-                    "A": 1,
-                    "2": 2,
-                    "3": 3,
-                    "4": 4,
-                    "5": 5,
-                    "6": 6,
-                    "7": 7,
-                    "8": 8,
-                    "9": 9,
-                    "10": 10,
-                    "J": 10,
-                    "Q": 10,
-                    "K": 10
-
-        
-            }
-            },
-            {
-            "suit": "Hearts",
-            "card": {
-                    "A": 1,
-                    "2": 2,
-                    "3": 3,
-                    "4": 4,
-                    "5": 5,
-                    "6": 6,
-                    "7": 7,
-                    "8": 8,
-                    "9": 9,
-                    "10": 10,
-                    "J": 10,
-                    "Q": 10,
-                    "K": 10
-            }
-            },
-            {
-            "suit": "Clubs",
-            "card": {
-                    "A": 1,
-                    "2": 2,
-                    "3": 3,
-                    "4": 4,
-                    "5": 5,
-                    "6": 6,
-                    "7": 7,
-                    "8": 8,
-                    "9": 9,
-                    "10": 10,
-                    "J": 10,
-                    "Q": 10,
-                    "K": 10
-            }
-            },
-            {
-            "suit": "Diamonds",
-            "card": {
-                    "A": 1,
-                    "2": 2,
-                    "3": 3,
-                    "4": 4,
-                    "5": 5,
-                    "6": 6,
-                    "7": 7,
-                    "8": 8,
-                    "9": 9,
-                    "10": 10,
-                    "J": 10,
-                    "Q": 10,
-                    "K": 10
-            }
-            }
-
-        
-    }
+    deck = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
-    
-    def deal():
-        return
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+    @classmethod
+    def shuffle(cls):
+        # shuffle the deck
+        random.shuffle(cls.deck)
+
+    @classmethod
+    def deal(cls):
+        # deal a card from the top of the deck
+        card = cls.deck.pop(0)
+        suit = cls.suits[random.randint(0, 3)]
+        return cls(suit, card)
 
 
+player_1 = Player(0)
+hand = Cards.deal()
 
-player_1 = Player()
+print(hand)
