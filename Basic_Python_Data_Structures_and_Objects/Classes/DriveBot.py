da82cb9c0e896fefbb9decb6bbb3cafee12e8429
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-09-15 19:41:40 -0700

    add disable_all

--- Basic_Python_Data_Structures_and_Objects/Classes/DriveBot.py
+++ Basic_Python_Data_Structures_and_Objects/Classes/DriveBot.py
@@ -2,6 +2,10 @@
 
 # Define the DriveBot class here!
 class DriveBot:
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+
     def __init__(self, motor_speed=0, direction=180, sensor_range=10):
         self.motor_speed = motor_speed
         self.direction = direction
@@ -14,6 +18,8 @@ class DriveBot:
 
     def adjust_sensor(self, new_sensor_range):
       self.sensor_range = new_sensor_range
+
+      
 
 robot_1 = DriveBot()
 # Use the methods here!
@@ -30,7 +36,16 @@ robot_1.sensor_range = 10
 
 # Create robot_2 here!
 robot_2 = DriveBot(35, 75, 25)
+robot_3 = DriveBot(20, 60, 10)
 
 print(robot_2.motor_speed)
 print(robot_2.direction)
 print(robot_2.sensor_range)
+
+DriveBot.longitude = -50.0
+DriveBot.latitude = 50.0
+DriveBot.all_disabled = True
+
+print(robot_1.latitude)
+print(robot_2.longitude)
+print(robot_3.all_disabled)
