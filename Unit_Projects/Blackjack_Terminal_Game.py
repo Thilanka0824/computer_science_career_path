8b7a904634c7d715a5a4885840235816c9b02e0b
Thilanka Rodrigo <thilanka@Thilankas-MacBook-Pro.local>
2023-09-16 19:29:53 -0700

    reworked classes

--- Unit_Projects/Blackjack_Terminal_Game.py
+++ Unit_Projects/Blackjack_Terminal_Game.py
@@ -1,37 +1,42 @@
-import random 
+import random
 
-class Player:
-    def __init__(self, score):
-        self.score = score
-    
-    
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = ("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
+values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, 
+          "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10,
+          "Queen": 10, "King": 10, "Ace": 11}
 
+playing = True
 
-class Cards:
-    deck = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+class Card:
 
-    def __init__(self, suit, value):
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.value = value
+        self.rank = rank
 
-    def __repr__(self):
-        return f"{self.value} of {self.suit}"
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+    
 
-    @classmethod
-    def shuffle(cls):
-        # shuffle the deck
-        random.shuffle(cls.deck)
+class Deck:
 
-    @classmethod
-    def deal(cls):
-        # deal a card from the top of the deck
-        card = cls.deck.pop(0)
-        suit = cls.suits[random.randint(0, 3)]
-        return cls(suit, card)
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self. deck.append(Card(suit, rank)) #build card objects and add it to the list
+    def __str__(self):
+        deck_comp = ""
+        for card in self.deck:
+            deck_comp += '\n ' + card.__str__() #add each Card object's print string
+        return 'The deck has:' + deck_comp
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
 
-
-player_1 = Player(0)
-hand = Cards.deal()
-
-print(hand)
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+    
+test_deck = Deck()
+print(test_deck)
