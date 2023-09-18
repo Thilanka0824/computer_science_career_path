fd6d1821c4b5dd0c0dfa2e32e4bb3abc044d883a
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-09-17 19:01:05 -0700

    implement linked list complete. test and review linked test

--- Intro_to_Data_Structures/Linked_Lists/linked_list_implementation.py
+++ Intro_to_Data_Structures/Linked_Lists/linked_list_implementation.py
@@ -46,3 +46,41 @@ ll.insert_beginning(70)
 ll.insert_beginning(5675)
 ll.insert_beginning(90)
 print(ll.stringify_list())
+
+my_node.set_next_node(99)
+print(my_node.get_next_node())
+
+  # Define your remove_node method below:
+def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+    
+def remove_multiple_nodes(self, value_to_remove):
+  current_node = self.get_head_node()
+
+
+
+trod_node = Node("Trod")
+lerman_node = Node("Katlin")
+bailey_node = Node("Bae")
+print(trod_node.get_value())
+print(lerman_node.get_value())
+print(bailey_node.get_value())
+
+fam_ll = LinkedList(1)
+fam_ll.insert_beginning(trod_node.get_value())
+fam_ll.insert_beginning(lerman_node.get_value())
+fam_ll.insert_beginning(bailey_node.get_value())
+print(fam_ll.stringify_list())
+
+fam_ll.remove_node(1)
+print(fam_ll.stringify_list())
